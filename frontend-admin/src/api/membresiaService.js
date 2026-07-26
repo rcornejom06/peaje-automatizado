652c77a4f294c88115cfca4e0ef500509a8054f2
rcornejom06 <mendezroger32@gmail.com>
2026-07-26 18:20:40 -0500

    Add functions to update and delete membership plans

--- frontend-admin/src/api/membresiaService.js
+++ frontend-admin/src/api/membresiaService.js
@@ -10,6 +10,16 @@ export const crearPlanMembresia = async (data) => {
   return response.data;
 };
 
+export const actualizarPlanMembresia = async (id, data) => {
+  const response = await api.patch(`/membresias/planes/${id}/`, data);
+  return response.data;
+};
+
+export const eliminarPlanMembresia = async (id) => {
+  const response = await api.delete(`/membresias/planes/${id}/`);
+  return response.data;
+};
+
 export const obtenerMembresias = async () => {
   const response = await api.get("/membresias/");
   return response.data;
