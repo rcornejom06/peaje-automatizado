c41969aada5b9f3c8f6505d9620f5e35f8d43820
rcornejom06 <mendezroger32@gmail.com>
2026-07-30 06:50:57 -0500

    Add functions to edit and change camera state

--- frontend-admin/src/api/camarasService.js
+++ frontend-admin/src/api/camarasService.js
@@ -9,3 +9,15 @@ export const crearCamara = async (data) => {
   const response = await api.post("/peajes/camaras/", data);
   return response.data;
 };
+
+export const editarCamara = async (id, data) => {
+  const response = await api.patch(`/peajes/camaras/${id}/`, data);
+  return response.data;
+};
+
+export const cambiarEstadoCamara = async (id, estado) => {
+  const response = await api.patch(`/peajes/camaras/${id}/cambiar-estado/`, {
+    estado,
+  });
+  return response.data;
+};
